484468eb7f7fa3b73e9236c271657b243e28275d
sevostyanovoleg106 <31765802+sevostyanovoleg106@users.noreply.github.com>
2017-09-13 18:19:17 +0500

    Update DROB.cpp

--- DROB.cpp
+++ DROB.cpp
@@ -1,24 +1,30 @@
-// DROB.cpp: определяет точку входа для консольного приложения.
-//
-
 #include "stdafx.h"
 #include <iostream>
 #include  <cmath> 
 
 using namespace std;
 
- double main()
+ int main()
 {
-	double x, y, z, a, b;
-	cin >> x;
-	cin >> y;
-	cin >> z;
-	a = ((2 * sin(y + PI / 3)) / (1 / 2 + pow(sin, 3)(pow(sin, 3)* abs(y + 2))) + pow(sin, -1)(x + PI / 2);
-	cout << "занечние а" << a << endl;
-	b = 1 + ((pow(z, 2) / 5) / (3 - pow(z, 2) / 3);
-	cout << "значение b" << b << endl;
+	 
+	 double x, y, z;
+	 double a=1, b=5;
+	 double PI = 3.14159265359;
+	 double r, t;
+
+	 cin >> x;
+	 cin >> y;
+	 cin >> z;
+	 r = pow(sin(abs(y+2)), 3);
+	 t = pow(sin(x+PI/2), -1);
+	 a = ((2 * sin(y + PI / 3)) / (1 / 2 + r)) + t;
+	 b = 1 + ((pow(z, 2) / 5) / 3 - pow(z, 2 / 3));
+	 cout << a<<endl;
+	 cout << b<<endl;
+
+
+
+
 	
 
      return 0;
-}
-
