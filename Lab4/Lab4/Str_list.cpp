aacf726c0e999cd6b656069c3ccf293108e3040f
Unknown <oleg.11.1999@mail.ru>
2017-12-23 21:01:32 +0500

    .
    .

---
+++ Lab4/Lab4/Str_list.cpp
@@ -0,0 +1,73 @@
+/*ќписать структуру с именем student, содержащую следующие пол€:
+Х	фамили€, им€, отчество;
+Х	номер группы;
+Х	успеваемость по 5 предметам (массив оценок).
+Ќаписать программу, выполн€ющую следующие действи€:
+Х	ввод с клавиатуры данных в массив, состо€щий из максимум 10 элементов типа student, 
+причем сделать возможность прервать ввод, чтобы можно было не вводить все 10 элементов;
+после окончани€ ввода отсортировать элементы массива по возрастанию номера группы;
+Х	вывод в консоль фамилий и инициалов, а также номеров групп всех студентов, включенных в массив, 
+если средний балл студента больше 4.0;
+Х	если таких студентов нет, вывести соответствующее сообщение;
+Х	реализовать вторую версию программы, вместо массива использовать двусв€зный список;
+элементы добавл€ть таким образом, чтобы сохран€лась упор€доченность списка по номерам групп 
+(вставка нового элемента после элемента, который меньше нового элемента и перед большим элементом).
+*/
+#include <windows.h>
+#include <iostream>
+using namespace std;
+
+struct student {
+	char* name = new char[128];
+	char* surName = new char[128];
+	char* middleName = new char[128];
+	int grade[5];
+	int number;
+	double sr = 0;
+};
+
+int main()
+{
+	setlocale(0, "");
+	student *sts = new student[10];
+	long int i = 0;
+
+	char a[10];
+	int n;
+	cin >> n;
+	for (int i = 0; i<n; i++)
+	{
+		cin >> sts[i].name >> sts[i].surName >> sts[i].middleName;
+		cin >> sts[i].number;
+		for (int j = 0; j < 5; j++) {
+			cin >> sts[i].grade[j];
+			sts[i].sr += sts[i].grade[j];
+		}
+		sts[i].sr /= 5;
+
+	}
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int j = 0; j < n - 1; j++)
+		{
+			if (sts[j].number > sts[j + 1].number)
+				swap(sts[j], sts[j + 1]);
+		}
+	}
+	bool isCheck = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (sts[i].sr > 4)
+		{
+			isCheck = 1;
+			cout << sts[i].name << " " << sts[i].surName << " " << sts[i].middleName << " " << sts[i].number << " ";
+			for (int j = 0; j < 5; j++)
+				cout << sts[i].grade[j] << " ";
+			cout << endl;
+		}
+	}
+	if (isCheck == 0)
+		cout << "vse studenti zavalili";
+
+	return 0;
+}
