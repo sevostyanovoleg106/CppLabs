f86b449756e20aace5de74bc6c192ca4b411c260
Unknown <oleg.11.1999@mail.ru>
2018-04-09 20:52:12 +0500

    .

--- Lab5/Lab5/Lab5.cpp
+++ Lab5/Lab5/Lab5.cpp
@@ -4,28 +4,18 @@ using namespace std;
 
 class Figur {
 public:
-	virtual void create()
-	{
-
-	}
-	virtual void move()
-	{
-
-	}
-	virtual void size()
-	{
-
-	}
-	virtual void output()
-	{
-
-	}
+	virtual void create() {}
+	virtual void move() {}
+	virtual void size() {}
+	virtual void output() {}
+	virtual void turn() {}
 
 protected:
 	int x, y, radius, dx, dy, dradius;
 };
 class circle : public Figur
 {
+	int x, y, radius, dx, dy, dradius, trn;
 public:
 	void create()
 	{
@@ -40,17 +30,14 @@ public:
 	}
 	void move()
 	{
-		
-		cout << "Введите значение координат вектора, на который хотите сдвинуть" << endl;
-		cout << "Значение по X: ";
 		cin >> dx;
-		cout << "Значение по Y: ";
 		cin >> dy;
 		x += dx;
 		y += dy;
 	}
 	void size()
 	{
+		bool flag = false;
 		int choice;
 		cout << "Уменьшить или увеличить размер?" << endl;
 		cout << "1) Увеличить" << endl;
@@ -66,16 +53,93 @@ public:
 			break;
 		case 2:
 			cout << "На сколько вы хотите уменьшить?" << endl;
-			cout << "Величена уменьшения: ";
-			cin >> dradius;
-			if (dradius > radius)
+		
+			while (!flag)
 			{
-				cout << "Значение уменьшения слишком большое. Введите повторно" << endl;
 				cout << "Величена уменьшения: ";
 				cin >> dradius;
+				if (dradius > radius)
+
+					cout << "Значение уменьшения слишком большое. Введите повторно" << endl;
+				
+				else
+				{
+					radius -= dradius;
+					flag = true;
+				}
 			}
-			else
-				radius -= dradius;
+			break;
+		default:
+			cout << "Неверно выбран пункт меню. Попробуйте снова" << endl;
+			size();
+			break;
+		}
+	}
+	void turn() 
+	{
+		cin >> trn;
+		cout << "Фигура повёрнута на " << trn << " градусов" << endl;
+	}
+	void output()
+	{
+		cout << "Координаты центра: " << x << " " << y << " " << "Радиус: " << radius << endl;
+	}
+};
+class square : public Figur
+{
+	int x, y, dx, dy, sideLength, dsideLength;
+public:
+	void create()
+	{
+		cout << "Введите координат центра" << endl;
+		cout << "По оси X: ";
+		cin >> x;
+		cout << "По оси Y: ";
+		cin >> y;
+		cout << "Введите значение стороны" << endl;
+		cout << "Размер: ";
+		cin >> sideLength;
+	}
+	void move()
+	{
+		cin >> dx;
+		cin >> dy;
+		x += dx;
+		y += dy;
+	}
+	void size()
+	{
+		int choice;
+		bool flag = false;
+		cout << "Уменьшить или увеличить размер?" << endl;
+		cout << "1) Увеличить" << endl;
+		cout << "2) Уменьшить" << endl;
+		cin >> choice;
+		switch (choice)
+		{
+		case 1:
+			cout << "На сколько вы хотите увеличить?" << endl;
+			cout << "Величена увеличения: ";
+			cin >> dsideLength;
+			sideLength += dsideLength;
+			break;
+		case 2:
+			cout << "На сколько вы хотите уменьшить?" << endl;
+			while (!flag)
+			{
+				cout << "Величена уменьшения: ";
+				cin >> dsideLength;
+				if (dsideLength > sideLength)
+
+					cout << "Значение уменьшения слишком большое. Введите повторно" << endl;
+
+				else
+				{
+					sideLength -= dsideLength;
+					flag = true;
+				}
+			}
+			
 			break;
 		default:
 			cout << "Неверно выбран пункт меню. Попробуйте снова" << endl;
@@ -85,16 +149,176 @@ public:
 	}
 	void output()
 	{
-		cout << "Координаты центра: " << x << " " << y << " " << "Радиус: " << radius << endl;
+		cout << "Координаты центра: " << x << " " << y << " " << "Сторона: " << sideLength << endl;
 	}
-};
-class square : public Figur
-{
-public:
 };
 class rectangle : public Figur
 {
+	int x, y, dx, dy, height, width, dheight, dwidth, dperimeter, x1, x2, y1, y2;
 public:
+	void create()
+	{
+		cout << "Введите координат центра" << endl;
+		cout << "По оси X: ";
+		cin >> x;
+		cout << "По оси Y: ";
+		cin >> y;
+		cout << "Введите ширину" << endl;
+		cout << "Ширина: ";
+		cin >> width;
+		cout << "Введите высоту" << endl;
+		cout << "Высота: ";
+		cin >> height;
+		x1 = x - width / 2;
+		x2 = x + width / 2;
+		y1 = y + height / 2;
+		y2 = y - height / 2;
+	}
+	void move()
+	{
+		cin >> dx;
+		cin >> dy;
+		x += dx;
+		y += dy;
+		x1 += dx;
+		x2 += dx;
+		y1 += dy;
+		y2 += dy;
+	}
+	void size()
+	{
+		int choice;
+		cout << "Уменьшить или увеличить размер?" << endl;
+		cout << "1) Увеличить" << endl;
+		cout << "2) Уменьшить" << endl;
+		cin >> choice;
+		bool flag = false;
+		switch (choice)
+		{
+		case 1:
+			cout << "Увеличить высоту или ширину?" << endl;
+			cout << "1)Высоту" << endl << "2)Ширину" << endl << "3)Высоту и ширину" << endl;
+			int chocie1;
+			cin >> chocie1;
+
+			while (!flag)
+			{
+				switch (chocie1)
+				{
+				case 1:
+					cout << "На сколько вы хотите увеличить?" << endl;
+					cout << "Величена увеличения: ";
+					cin >> dheight;
+					height += dheight;
+					flag = true;
+					break;
+
+				case 2:
+					cout << "На сколько вы хотите увеличить?" << endl;
+					cout << "Величена увеличения: ";
+					cin >> dwidth;
+					width += dwidth;
+					flag = true;
+					break;
+				case 3:
+					cout << "На сколько вы хотите увеличить?" << endl;
+					cout << "Величена увеличения высоты: ";
+					cin >> dperimeter;
+					height += dperimeter;
+					width += dperimeter;
+					flag = true;
+					break;
+				default:
+					cout << "Неверно выбран пункт меню. Попробуйте снова" << endl;
+					break;
+				}
+			}
+			break;
+		case 2:
+			cout << "Уменьшить высоту или ширину?" << endl;
+			cout << "1)Высоту" << endl << "2)Ширину" << endl << "3)Высоту и ширину" << endl;
+			int chocie;
+			cin >> chocie;
+			while (!flag)
+			{
+				bool flag1 = false;
+				switch (chocie)
+				{
+				case 1:
+					cout << "На сколько вы хотите уменьшить?" << endl;
+					while (!flag1)
+					{
+						cout << "Величена уменьшения: ";
+						cin >> dheight;
+						if (dheight > height)
+						{
+							cout << "Значение уменьшения слишком большое. Введите повторно" << endl;
+
+						}
+						else
+						{
+							height -= dheight;
+							flag1 = true;
+							flag = true;
+						}
+					}
+					break;
+				case 2:
+				overwriting2:
+					cout << "На сколько вы хотите уменьшенить?" << endl;
+					while (!flag1)
+					{
+						cout << "Величена уменьшения: ";
+						cin >> dwidth;
+						if (dwidth > width)
+						{
+							cout << "Значение уменьшения слишком большое. Введите повторно" << endl;
+
+						}
+						else
+						{
+							width -= dwidth;
+							flag1 = true;
+							flag = true;
+						}
+					}
+					break;
+				case 3:
+					cout << "На сколько вы хотите уменьшить?" << endl;
+					while (!flag1)
+					{
+						cout << "Величена уменьшения: ";
+						cin >> dperimeter;
+						if (dperimeter > width || dperimeter >height)
+						{
+							cout << "Значение уменьшения слишком большое. Введите повторно" << endl;
+
+						}
+						else
+						{
+							height -= dperimeter;
+							width -= dperimeter;
+							flag1 = true;
+							flag = true;
+						}
+					}
+					break;
+				default:
+					cout << "Неверно выбран пункт меню. Попробуйте снова" << endl;
+					break;
+				}
+			}
+			break;
+		default:
+			cout << "Неверно выбран пункт меню. Попробуйте снова" << endl;
+			size();
+			break;
+		}
+	}
+	void output()
+	{
+		cout << "Координаты центра: " << x << " " << y << " " << "Высота: " << height << " " << "Ширина: " << width << endl;
+	}
 };
 int main()
 {
@@ -146,30 +370,33 @@ int main()
 
 		F[next]->create();
 		int postmenu;
-		cout << "Выберите функцию" << endl << "1)Перемещение" << endl << "2)Изменение размера" <<endl<<"3)Закончить"<< endl;
-		
 		bool flag = false;
+		
 		while (!flag)
 		{
+			cout << "Выберите функцию" << endl << "1)Перемещение" << endl << "2)Изменение размера" << endl << "3)Поворот" << endl << "4)Закончить" << endl;
 			cin >> postmenu;
 			switch (postmenu)
 			{
 			case 1:
+				cout << "Введите значение координат вектора, на который хотите сдвинуть" << endl;
+				cout << "Значение по X и по Y: ";
 				F[next]->move();
 				F[next]->output();
-				cout << "Выберите функцию" << endl << "1)Перемещение" << endl << "2)Изменение размера" << endl << "3)Закончить" << endl;
 				break;
 			case 2:
 				F[next]->size();
 				F[next]->output();
-				cout << "Выберите функцию" << endl << "1)Перемещение" << endl << "2)Изменение размера" << endl << "3)Закончить" << endl;
 				break;
 			case 3:
+				cout << "Введите значение поворота в градусах " << endl;
+				F[next]->turn();
+				break;
+			case 4:
 				flag = true;
 				break;
 			default:
 				cout << "Неверный ввод. Попробуйте снова" << endl;
-				cout << "Выберите функцию" << endl << "1)Перемещение" << endl << "2)Изменение размера" << endl << "3)Закончить" << endl;
 				break;
 			}
 		}
