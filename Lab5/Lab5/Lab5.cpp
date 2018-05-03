102ea14186537201fdd54418246b5197f97e393c
Unknown <oleg.11.1999@mail.ru>
2018-05-04 03:24:16 +0500

    .

--- Lab5/Lab5/Lab5.cpp
+++ Lab5/Lab5/Lab5.cpp
@@ -9,7 +9,8 @@ public:
 
 	virtual void create() {}
 	virtual void move(double dx, double dy) {}
-	virtual void size() {}
+	virtual void increase(int chocie1, double delta) {}
+	virtual void decrease(int chocie1, double delta) {}
 	virtual void output() {}
 	virtual void rotation() {}
 protected:
@@ -18,9 +19,9 @@ protected:
 };
 class circle : public Figur
 {
-	double x, y, radius, trn, dradius, dx, dy;
+	double  dradius, dx, dy;
 public:
-
+	double x, y, radius, trn ;
 
 	circle(double x, double y, double radius)
 	{
@@ -49,7 +50,7 @@ public:
 				if (dradius > radius)
 
 					flag = false;
-		
+
 				else
 				{
 					radius -= dradius;
@@ -57,8 +58,8 @@ public:
 				}
 			}
 		}
-		
-		
+
+
 	}
 	void rotation(double trn)
 	{
@@ -70,15 +71,16 @@ public:
 };
 class square : public Figur
 {
-	double x, y, dx, dy, sideLength, dsideLength, trn, x1, x2, y1, y2, halfDiagonal, PI = 3.1415926535;
+	double x, y, dx, dy, sideLength, dsideLength, trn, halfDiagonal, PI = 3.1415926535;
 public:
-
+	double x1, y1, x2, y2;
 	square(double x, double y, double sideLength)
 	{
 		this->x = x;
 		this->y = y;
 		this->sideLength = sideLength;
 		halfDiagonal = sqrt((sideLength / 2)*(sideLength / 2) * 2);
+
 		x1 = x - sideLength / 2;
 		y1 = y + sideLength / 2;
 		x2 = x + sideLength / 2;
@@ -89,6 +91,10 @@ public:
 	{
 		x += dx;
 		y += dy;
+		x1 = x - sideLength / 2;
+		y1 = y + sideLength / 2;
+		x2 = x + sideLength / 2;
+		y2 = y - sideLength / 2;
 	}
 	void size(int chocie, double dsideLength)
 	{
@@ -112,7 +118,7 @@ public:
 				}
 			}
 		}
-		
+
 	}
 	void rotation(double trn)
 	{
@@ -129,9 +135,10 @@ public:
 };
 class rectangle : public Figur
 {
-	double trn, x, y, dx, dy, height, width, dheight, dwidth, dperimeter, x1, x2, y1, y2, PI = 3.1415926535, halfDiagonal;
+	double trn, x, y, dx, dy, height, width, dheight, dwidth, dperimeter, PI = 3.1415926535, halfDiagonal, perimeter;
 
 public:
+	double  x1, x2, y1, y2;
 	rectangle(double x, double y, double width, double height)
 	{
 		this->x = x;
@@ -143,6 +150,7 @@ public:
 		y1 = y + height / 2;
 		y2 = y - height / 2;
 		halfDiagonal = sqrt(pow(width / 2, 2) + pow(height / 2, 2));
+		perimeter = width + height;
 		identifier = 3;
 	}
 	void move(double dx, double dy)
@@ -154,137 +162,51 @@ public:
 		y1 += dy;
 		y2 += dy;
 	}
-	void size()
+	void increase(int choice, double delta)
 	{
 
-		/*int choice;
-		cout << "Уменьшить или увеличить размер?" << endl;
-		cout << "1) Увеличить" << endl;
-		cout << "2) Уменьшить" << endl;
-		cin >> choice;
-		bool flag = false;
-		switch (choice)
+		if (choice == 1)
 		{
-		case 1:
-			cout << "Увеличить высоту или ширину?" << endl;
-			cout << "1)Высоту" << endl << "2)Ширину" << endl << "3)Высоту и ширину" << endl;
-			int chocie1;
-			cin >> chocie1;
-
-			while (!flag)
-			{
-				switch (chocie1)
-				{
-				case 1:
-					cout << "На сколько вы хотите увеличить?" << endl;
-					cout << "Величена увеличения: ";
-					cin >> dheight;
-					height += dheight;
-					flag = true;
-					break;
-
-				case 2:
-					cout << "На сколько вы хотите увеличить?" << endl;
-					cout << "Величена увеличения: ";
-					cin >> dwidth;
-					width += dwidth;
-					flag = true;
-					break;
-				case 3:
-					cout << "На сколько вы хотите увеличить?" << endl;
-					cout << "Величена увеличения высоты: ";
-					cin >> dperimeter;
-					height += dperimeter;
-					width += dperimeter;
-					flag = true;
-					break;
-				default:
-					cout << "Неверно выбран пункт меню. Попробуйте снова" << endl;
-					break;
-				}
-			}
-			break;
-		case 2:
-			cout << "Уменьшить высоту или ширину?" << endl;
-			cout << "1)Высоту" << endl << "2)Ширину" << endl << "3)Высоту и ширину" << endl;
-			int chocie;
-			cin >> chocie;
-			while (!flag)
-			{
-				bool flag1 = false;
-				switch (chocie)
-				{
-				case 1:
-					cout << "На сколько вы хотите уменьшить?" << endl;
-					while (!flag1)
-					{
-						cout << "Величена уменьшения: ";
-						cin >> dheight;
-						if (dheight > height)
-						{
-							cout << "Значение уменьшения слишком большое. Введите повторно" << endl;
-
-						}
-						else
-						{
-							height -= dheight;
-							flag1 = true;
-							flag = true;
-						}
-					}
-					break;
-				case 2:
-				overwriting2:
-					cout << "На сколько вы хотите уменьшенить?" << endl;
-					while (!flag1)
-					{
-						cout << "Величена уменьшения: ";
-						cin >> dwidth;
-						if (dwidth > width)
-						{
-							cout << "Значение уменьшения слишком большое. Введите повторно" << endl;
-
-						}
-						else
-						{
-							width -= dwidth;
-							flag1 = true;
-							flag = true;
-						}
-					}
-					break;
-				case 3:
-					cout << "На сколько вы хотите уменьшить?" << endl;
-					while (!flag1)
-					{
-						cout << "Величена уменьшения: ";
-						cin >> dperimeter;
-						if (dperimeter > width || dperimeter >height)
-						{
-							cout << "Значение уменьшения слишком большое. Введите повторно" << endl;
-
-						}
-						else
-						{
-							height -= dperimeter;
-							width -= dperimeter;
-							flag1 = true;
-							flag = true;
-						}
-					}
-					break;
-				default:
-					cout << "Неверно выбран пункт меню. Попробуйте снова" << endl;
-					break;
-				}
-			}
-			break;
-		default:
-			cout << "Неверно выбран пункт меню. Попробуйте снова" << endl;
-			size();
-			break;
-		}*/
+			height += delta;
+		}
+		else if (choice == 2)
+		{
+			width += delta;
+		}
+		else
+		{
+			perimeter += delta;
+		}
 	}
+	void decrease(int choice, double delta)
+	{
+		while (1)
+		{
+			if (choice == 1)
+			{
+				if (delta > height)
+					height -= delta;
+				else
+					break;
+			}
+			else if (choice == 2)
+			{
+				if (delta > height)
+					width -= delta;
+				else
+					break;
+			}
+			else
+			{
+				if (delta > height)
+					perimeter -= delta;
+				else
+					break;
+
+			}
+		}
+	}
+	
 	void rotation()
 	{
 
@@ -331,30 +253,10 @@ public:
 		x_2 = x2;
 		y_1 = y1;
 		x_2 = x2;
-		cout << trn << endl;
-		cout << cos(trn) << ", " << sin(PI) << endl;
-
-		cout << "Координаты левого верхнего угла: " << x1 << ", " << y1 << endl;
-		cout << "Координаты правого нижнего угла: " << x2 << ", " << y2 << endl;
-
 		trn = 0;
-	}
-	void intersection()
-	{
-
-	}
-	void output()
-	{
-		cout << "Координаты центра: " << x << " " << y << " " << "Высота: " << height << " " << "Ширина: " << width << endl;
 	}
 };
 
-/*void output()
-{
-cout << "Координаты центра: " << x << " " << y << " " << "Сторона: " << sideLength << endl;
-cout << "Координаты левого верхнего угла: " << x1 << ", " << y1 << endl;
-cout << "Координаты правого нижнего угла: " << x2 << ", " << y2 << endl;
-}*/
 int main()
 {
 	setlocale(0, "");
@@ -367,104 +269,143 @@ int main()
 	Figur *sq = s;
 	rectangle * r = new rectangle(0, 0, 0, 0);
 	Figur *rect = r;
+	int x, y, radius, sideLength, dx, dy, height, width;
+	while (1)
+	{
+		flag1 = false;
+		while (!flag1)
+		{
+			int menu;
+			cout << "Выбeрете тип фигуры" << endl;
+			cout << "1) Круг" << endl << "2) Квадрат" << endl << "3) Прямоугольник" << endl;
+			cin >> menu;
+			switch (menu)
+			{
 
-	//while (1)
-	//{
-	//	flag1 = false;
-	//	while (!flag1)
-	//	{
-	//		int menu;
-	//		cout << "Выбeрете тип фигуры" << endl;
-	//		cout << "1) Круг" << endl << "2) Квадрат" << endl << "3) Прямоугольник" << endl;
-	//		cin >> menu;
-	//		switch (menu)
-	//		{
+			case 1:
+				next++;
+				cir = c;
+				F[next] = cir;
+				cout << "Введите координат центра" << endl;
+				cout << "По оси X: ";
+				cin >> x;
+				cout << "По оси Y: ";
+				cin >> y;
+				cout << "Введите значение радиуса" << endl;
+				cout << "Размер: ";
+				cin >> radius;
+				circle(x, y, radius);
+				cout << "Координаты центра: " << x << " " << y << " " << "Радиус: " << radius << endl;
+				
+				flag1 = true;
+				break;
+			case 2:
+				next++;
+				sq = s;
+				F[next] = sq;
+				cout << "Введите координат центра" << endl;
+				cout << "По оси X: ";
+				cin >> x;
+				cout << "По оси Y: ";
+				cin >> y;
+				cout << "Введите значение стороны" << endl;
+				cout << "Размер: ";
+				cin >> sideLength;
+				square(x, y, sideLength);
+				cout << "Координаты центра: " << x << " " << y << " " << "Сторона: " << sideLength << endl;
+				cout << "Координата левого верхнего угла: " << s->x1 << ", " << s->y1 << endl;
+				cout << "Координата правого нижнего угла: " << s->x2 << ", " << s->y2 << endl;
+				flag1 = true;
+				break;
+			case 3:
+				next++;
+				rect = r;
+				F[next] = rect;
+				cout << "Введите координат центра" << endl;
+				cout << "По оси X: ";
+				cin >> x;
+				cout << "По оси Y: ";
+				cin >> y;
+				cout << "Введите значение высоты" << endl;
+				cout << "Размер: ";
+				cin >> height;
+				cout << "Введите значение ширины" << endl;
+				cout << "Размер: ";
+				cin >> width;
+				rectangle(x, y, width, height);
+				cout << "Координаты центра: " << x << " " << y << " " << "Высота: " << height<< ' ' << "Ширина: " << width << endl;
+				cout << "Координата левого верхнего угла: " << r->x1 << ", " << r->y1 << endl;
+				cout << "Координата правого нижнего угла: " << r->x2 << ", " << r->y2 << endl;
+				flag1 = true;
+				break;
+			default:
+				cout << "Неверный ввод. Попробуйте снова" << endl;
+				break;
 
-	//		case 1:
-	//			next++;
-	//			cir = c;
-	//			F[next] = cir;
-	//			cout << "Введите координат центра" << endl;
-	//			cout << "По оси X: ";
-	//			cin >> c->x;
-	//			cout << "По оси Y: ";
-	//			cin >> c->y;
-	//			cout << "Введите значение радиуса" << endl;
-	//			cout << "Размер: ";
-	//			cin >> c->radius;
-	//			circle(c->x, c->y, c->radius);
-	//			cout << "Координаты центра: " << c->x << " " << c->y << " " << "Радиус: " << c->radius << endl;
-	//			flag1 = true;
-	//			break;
-	//		case 2:
-	//			next++;
-	//			sq = s;
-	//			F[next] = sq;
-	//			cout << "Введите координат центра" << endl;
-	//			cout << "По оси X: ";
-	//			cin >> s->x;
-	//			cout << "По оси Y: ";
-	//			cin >> s->y;
-	//			cout << "Введите значение стороны" << endl;
-	//			cout << "Размер: ";
-	//			cin >> s->sideLength;
-	//			square(s->x, s->y, s->sideLength);
-	//			cout << "Координаты центра: " << s->x << " " << s->y << " " << "Сторона: " << s->sideLength << endl;
-	//			cout << "Координаты левого верхнего угла: " << s->x1 << ", " << s->y1 << endl;
-	//			cout << "Координаты правого нижнего угла: " << s->x2 << ", " << s->y2 << endl;
-	//			flag1 = true;
-	//			break;
-	//		case 3:
-	//			next++;
-	//			rect = r;
-	//			F[next] = rect;
-	//			F[next]->output();
-	//			flag1 = true;
-	//			break;
-	//		default:
-	//			cout << "Неверный ввод. Попробуйте снова" << endl;
-	//			break;
-	//		}
-	//	}
+			}
+		}
 
-	//	int postmenu;
-	//	bool flag = false;
+		int postmenu;
+		bool flag = false;
 
-	//	while (!flag)
-	//	{
-	//		cout << "Выберите функцию" << endl << "1)Перемещение" << endl << "2)Изменение размера" << endl << "3)Поворот" << endl << "4)Закончить" << endl;
-	//		cin >> postmenu;
-	//		switch (postmenu)
-	//		{
-	//		case 1:
-	//			cout << "Введите значение координат вектора, на который хотите сдвинуть" << endl;
-	//			cout << "Значение по X и по Y: ";
+		while (!flag)
+		{
+			cout << "Выберите функцию" << endl << "1)Перемещение" << endl << "2)Изменение размера" << endl << "3)Поворот" << endl << "4)Закончить" << endl;
+			cin >> postmenu;
+			switch (postmenu)
+			{
+			case 1:
+				cout << "Введите значение координат вектора, на который хотите сдвинуть" << endl;
+				cout << "Значение по X : ";
+				cin >> dx;
+				cout << "Занчение по Y : ";
+				cin >> dy;
+				F[next]->move(dx, dy);
+				cout << x << ", " << y << endl;
+				break;
+			case 2:
+				int choice;
+				cout << "Уменьшить или увеличить размер?" << endl;
+				cout << "1) Увеличить" << endl;
+				cout << "2) Уменьшить" << endl;
+				cin >> choice;
+				cout << "Увеличить высоту или ширину?" << endl;
+				cout << "1)Высоту" << endl << "2)Ширину" << endl << "3)Высоту и ширину" << endl;
+				int chocie1;
+				cin >> chocie1;
 
-	//			cin >> F[next]->dx;
-	//			cin >> F[next]->dy;
-	//			F[next]->move(F[next]->dx, F[next]->dy);
-	//			F[next]->output();
-	//			break;
-	//		case 2:
-	//			F[next]->size();
-	//			F[next]->output();
-	//			break;
-	//		case 3:
-	//			cout << "Введите значение угла поворота в градусах " << endl;
-	//			cin >> F[next]->trn;
-	//			F[next]->turn();
-	//			F[next]->output();
-	//			break;
-	//		case 4:
-	//			flag = true;
-	//			break;
-	//		default:
-	//			cout << "Неверный ввод. Попробуйте снова" << endl;
-	//			break;
-	//		}
-	//	}
-	//}
+				if (choice == 1)
+				{
+					cout << "Ведите величену увеличения " << endl;
+					double delta;
+					cin >> delta;
+					F[next]->increase(chocie1, delta);
+					cout << "Высота: " << height<<' '<< "Ширина: " << width <<' '<< "Пириметр: " << endl;
+				}
+				else
+				{
+					cout << "Ведите величену уменьшения " << endl;
+					double delta;
+					cin >> delta;
+					F[next]->decrease(chocie1, delta);
+					cout << "Высота: " << height << ' ' << "Ширина: " << width << ' ' << "Пириметр: " << endl;
+				}
+				break;
+				/*	case 3:
+						cout << "Введите значение угла поворота в градусах " << endl;
+						cin >> F[next]->trn;
+						F[next]->turn();
+						F[next]->output();
+						break;*/
+			case 4:
+				flag = true;
+				break;
+			default:
+				cout << "Неверный ввод. Попробуйте снова" << endl;
+				break;
+			}
+		}
+	}
 
-	//system("pause");
 	return 0;
 }
