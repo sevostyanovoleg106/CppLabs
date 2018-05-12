876ba74ef738d79a743b2aa77e33f739d5cd8e4c
Unknown <oleg.11.1999@mail.ru>
2018-05-12 23:09:13 +0500

    Комплит

--- Lab6/Lab6/Lab6.cpp
+++ Lab6/Lab6/Lab6.cpp
@@ -1,7 +1,14 @@
-#include <iostream>
+п»ї#include <iostream>
 
 using namespace std;
 
+void output(double x, double y)
+{
+	if (y < 0)
+		cout << "z = " << x << y << (char)251 << "-1" << endl;
+	else
+		cout << "z = " << x << "+" << y << (char)251 << "-1" << endl;
+}
 
 class ComplexNumber
 {
@@ -13,45 +20,55 @@ public:
 		x = copyCN.x;
 		y = copyCN.y;
 	}
-	void inputNumber(char* stroka, double& x, double& y)
+	void inputNumber(char* stroka)
 	{
 		char* strX = new char[strlen(stroka)];
 		char* strY = new char[strlen(stroka)];
 		int i = 0;
-		for ( i; i < strlen(stroka); i++)
+		bool sign = false;
+		for (i; i < strlen(stroka); i++)
 		{
+
+			if (stroka[i] == '-' && sign == false)
+			{
+				strX[i] = '-';
+				i++;
+				sign = true;
+			}
 			if (stroka[i] == '+' || stroka[i] == '-')
 				break;
 			else
 				strX[i] = stroka[i];
-				
-
 		}
 		int f = 0;
-		for (++i; f < strlen(stroka); f++,i++)
+		for (++i; f < strlen(stroka); f++, i++)
 		{
+			if (stroka[i - 1] == '-')
+			{
+				strY[f] = '-';
+				f++;
+			}
+
 			strY[f] = stroka[i];
 		}
 		x = atof(strX);
 		y = atof(strY);
 	}
-	void outputNumber(char* stroka, double& x, double& y)
+	void set_x(double x)
 	{
-		bool sign;
-		for (int i = 0; i < strlen(stroka); i++)
-		{
-			if (stroka[i] == '+')
-			{
-				sign = true;
-				break;
-			}
-			else
-				sign = false;
-		}
-		if (sign == true)
-			cout << "z = " << x << '+' << y << '!' << "-1" << endl;
-		else
-			cout << "z = " << x << '-' << y << '!' << "-1" << endl;
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
+	double get_x()
+	{
+		return x;
+	}
+	double get_y()
+	{
+		return y;
 	}
 	const ComplexNumber operator+(const ComplexNumber sum) const
 	{
@@ -73,46 +90,62 @@ public:
 	{
 		ComplexNumber result;
 		result.x = x * composition.x;
-		result.y = y * composition.y;
+		result.y = y * composition.x;
 		return (result);
 	}
 
 };
 int main()
 {
-	setlocale(0, "");
-	ComplexNumber z1,z2,zr;
-	cout << "Введите комплексное число" << endl;
-	char* stroka = new char[256];
-	cin >> stroka;
-	double x, y;
-	z1.inputNumber(stroka,x,y);
+	ComplexNumber z1, z2, zr;
+
 	int menu;
 	char* stroka1 = new char[256];
+	
 	while (1)
 	{
-		cout << "Выберите операцию" << endl;
-		cout << "1) Сложение комплексных чисел " << endl << "2) Вычитание комплексных чисел " << endl << "3) Умножение на число " << endl;
-		cin >> menu;
-		switch (menu)
+		bool flag = false;
+		cout << "Enter a complex number in the form x +/- y" << endl;
+		char* stroka = new char[256];
+		cin >> stroka;
+		z1.inputNumber(stroka);
+		while (!flag)
 		{
-		case 1:
-			cout << "Введите число, с которым хотите сложить" << endl;
-			cin >> stroka1;
-			z2.inputNumber(stroka1, x, y);
-			zr = z1 + z2;
-			zr.outputNumber(stroka1, x, y);
-			break;
-		case 2:
-			zr = z1 - z2;
-			z1.outputNumber(stroka, x, y);
-			break;
-		case 3:
-			zr = z1 + z2;
-			z1.outputNumber(stroka, x, y);
-			break;
-		default:
-			break;
+
+
+			cout << "Select an operation" << endl;
+			cout << "1) Addition of complex numbers " << endl << "2) Subtraction of complex numbers " << endl << "3) Multiplication by a number "<<endl<<"4) Back" << endl;
+			cin >> menu;
+			switch (menu)
+			{
+			case 1:
+				cout << "Enter the number you want to add to" << endl;
+				cin >> stroka1;
+				z2.inputNumber(stroka1);
+				zr = z1 + z2;
+				output(zr.get_x(), zr.get_y());
+				break;
+			case 2:
+				cout << "Enter the number you want to take away from" << endl;
+				cin >> stroka1;
+				z2.inputNumber(stroka1);
+				zr = z1 - z2;
+				output(zr.get_x(), zr.get_y());
+				break;
+			case 3:
+				cout << "Enter the number you want to take away from" << endl;
+				cin >> stroka1;
+				z2.inputNumber(stroka1);
+				zr = z1 * z2;
+				output(zr.get_x(), zr.get_y());
+				break;
+			case 4:
+				flag = true;
+				break;
+			default:
+				cout << "Try again" << endl;
+				break;
+			}
 		}
 	}
 	return 0;
